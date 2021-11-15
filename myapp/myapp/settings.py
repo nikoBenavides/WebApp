0be5d624869e119781ba071d133dfabaa162f88b
nikoBenavides <80792944+nikoBenavides@users.noreply.github.com>
2021-11-14 21:17:59 -0500

    Web Deplyment pt1

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g9te%oi*1$&o=!+phk^n+t2mzfq4^(l*2!vtzh!j4yl6#b#ij+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com'']
 
 
 # Application definition
@@ -45,6 +45,7 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -16,10 +16,17 @@ Including another URLconf
 from django import urls
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import  settings
+
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include ('app_crud.urls'))
+    path('',include ('app_crud.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
    
     
 ]
